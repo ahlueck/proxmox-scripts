cd84a491523f9d44d369e65a7e8af602aa0ce2cd
ahlueck <147993537+ahlueck@users.noreply.github.com>
2023-11-26 16:29:11 +0100

    Update debian.sh

--- lxc/nginx-proxy-manager/install/debian.sh
+++ lxc/nginx-proxy-manager/install/debian.sh
@@ -101,10 +101,9 @@ runcmd apt-get update && apt-get install -y -q --no-install-recommends openresty
 log "Installing nodejs"
 runcmd curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_16.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-touch /etc/apt/preferences.d/nodejs-NginxProxy
-echo "Package: nodejs" | tee /etc/apt/preferences.d/nodejs-NginxProxy
-echo "Pin: origin deb.nodesource.com" | tee /etc/apt/preferences.d/nodejs-NginxProxy
-echo "Pin-Priority: 900" | tee /etc/apt/preferences.d/nodejs-NginxProxy
+echo "Package: nodejs" | tee -a /etc/apt/preferences.d/nodejs-NginxProxy
+echo "Pin: origin deb.nodesource.com" | tee -a /etc/apt/preferences.d/nodejs-NginxProxy
+echo "Pin-Priority: 900" | tee -a /etc/apt/preferences.d/nodejs-NginxProxy
 runcmd apt-get update
 runcmd apt-get install -y nodejs
 runcmd npm install --global yarn
